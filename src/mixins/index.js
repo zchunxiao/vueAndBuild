2c92af16b27d0d6d76095a8895a993ac7f375d21
zhangcx <2454607455@qq.com>
2021-10-16 08:28:36 +0800

    1

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,17 +1,17 @@
 export default {
-  data(){
-      return {
-          mixinData:"我是混入的数据"
-      }
-  },
-  props:{},
-  mounted() {
-      console.log('this:',this)
-
-  },
-  methods: {
-      mixinMethod1(){
-          console.log("我是混入的数据")
-      }
-  },
+    data() {
+        return {
+            mixinData: "我是混入的数据"
+        }
+    },
+    props: {},
+    mounted() {
+        console.log('this:', this);-
+        console.log("ddd:",this.fullNameByMethod());
+    },
+    methods: {
+        mixinMethod1() {
+            console.log("我是混入的数据")
+        }
+    },
 }
